c2a87334b8383d3f11c6d5281522ec374b7e0d4f
Utkarsh Ashok Pathrabe <uapathrabe@gmail.com>
2021-12-19 22:22:46 +0530

    Create Projects.js

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
+import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import { projects } from '../../constants/constants';
+
+const Projects = () => (
+  <Section nopadding id='projects'>
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map(( { id, image, title, description, tags, source, visit } ) => (
+        <BlogCard key={id}>
+          <Img src={image} />
+          <TitleContent>
+            <HeaderThree title>
+              {title}
+            </HeaderThree>
+            <Hr />
+          </TitleContent>
+          <CardInfo>{description}</CardInfo>
+          <div>
+            <TitleContent>Stack</TitleContent>
+            <TagList>
+              {tags.map((tag, index) => (
+                <Tag key={index}>{tag}</Tag>
+              ))}
+            </TagList>
+          </div>
+          <UtilityList>
+            <ExternalLinks href={visit} target='_blank'>
+              Visit
+            </ExternalLinks>
+            <ExternalLinks href={source} target='_blank'>
+              Source
+            </ExternalLinks>
+          </UtilityList>
+        </BlogCard>
+      ))}
+    </GridContainer>
+  </Section>
+);
+
+export default Projects;
