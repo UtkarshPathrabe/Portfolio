d119a63277bdad46d4d4b27c7b2156b718aff8fc
Utkarsh Ashok Pathrabe <uapathrabe@gmail.com>
2021-12-20 16:18:13 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import Acomplishments from '../components/Acomplishments/Acomplishments';
 import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
 import Hero from '../components/Hero/Hero';
@@ -9,16 +11,26 @@ import { Section } from '../styles/GlobalComponents';
 
 const Home = () => {
   return (
-    <Layout>
-      <Section grid>
-        <Hero />
-        <BgAnimation />
-      </Section>
-      <Projects />
-      <Technologies />
-      <Timeline />
-      <Acomplishments />
-    </Layout>
+    <>
+      <Head>
+        <meta charset="utf-8"></meta>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport"></meta>
+        <title>Utkarsh Pathrabe</title>
+        <meta content="Utkarsh Pathrabe's personal website" name="description"></meta>
+        <meta content="utkarsh pathrabe reactjs python java sql coder webdeveloper" name="keywords"></meta>
+        <link href="/images/logo.png" rel="icon"></link>
+      </Head>
+      <Layout>
+        <Section grid>
+          <Hero />
+          <BgAnimation />
+        </Section>
+        <Projects />
+        <Technologies />
+        <Timeline />
+        <Acomplishments />
+      </Layout>
+    </>
   );
 };
 
