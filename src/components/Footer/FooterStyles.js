4174dc635fb8c0848f230e343262d0225a97f0ae
Utkarsh Pathrabe <uapathrabe@gmail.com>
2021-12-31 20:51:26 +0530

    Update FooterStyles.js

--- src/components/Footer/FooterStyles.js
+++ src/components/Footer/FooterStyles.js
@@ -1,12 +1,11 @@
 import styled from "styled-components"
 
 export const FooterWrapper = styled.section`
-	width: calc(100vw - 96px);
-  max-width: 1040px;
+  width: calc(100% - 96px);
+  max-width: 1096px;
   padding: 2rem 48px 40px;
   margin: 1rem auto;
   box-sizing: content-box;
-
 
   @media ${props => props.theme.breakpoints.sm} {
     padding: 0 16px 48px;
