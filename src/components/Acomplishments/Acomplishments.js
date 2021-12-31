3e2716d20323bfca1d5e59550e96d7381bf2aff8
Utkarsh Ashok Pathrabe <uapathrabe@gmail.com>
2021-12-31 13:11:16 +0530

    Update Acomplishments.js

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -5,7 +5,7 @@ import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
   { number: 20, text: 'Open Source Projects'},
-  { number: 180, text: 'Github Followers', },
+  { number: 190, text: 'Github Followers', },
   { number: 390, text: 'Github Stars', }
 ];
 
