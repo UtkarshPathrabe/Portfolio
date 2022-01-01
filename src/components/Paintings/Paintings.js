dc06b0b203badd2318f4c6801f4f81545bcb5dd2
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-01 19:40:34 +0530

    Create Paintings.js

---
+++ src/components/Paintings/Paintings.js
@@ -0,0 +1,16 @@
+import Gallery from "react-photo-gallery";
+import { Section, SectionDivider, SectionTitle, SectionText } from '../../styles/GlobalComponents';
+import { paintings } from '../../constants/paintings';
+
+const Paintings = () => {
+    return (
+        <Section id='paintings'>
+            <SectionDivider />
+            <SectionTitle main>Paintings</SectionTitle>
+            <SectionText>Some of my paintings...</SectionText>
+            <Gallery photos={paintings} />
+        </Section>
+    );
+};
+
+export default Paintings;
