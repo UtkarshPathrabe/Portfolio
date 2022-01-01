206f8f467dd06a1834b4cce00a718f2846bd26b5
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-01 19:40:36 +0530

    Create PaintingsStyles.js

---
+++ src/components/Paintings/PaintingsStyles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ImgContainer = styled.div`
+    margin: 4px 20px;
+`;
+
+export const Img = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    overflow: hidden;
+    background: #edf6ff;
+    border-radius: 10px;
+`;
