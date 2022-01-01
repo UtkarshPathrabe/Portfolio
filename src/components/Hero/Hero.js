cf5da352c62e428dc75f54d48e1f2ffe28f73570
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-01 17:01:34 +0530

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Link from 'next/link';
 
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <Section row nopadding>
-    <LeftSection>
-      <SectionTitle main center>
-        Utkarsh Pathrabe
-      </SectionTitle>
-      <SectionText>
-        Senior System Software Engineer (Autonomous Vehicles) at NVIDIA
-      </SectionText>
-      <Link href='#about' passHref>
-        <Button>Learn More</Button>
-      </Link>
-    </LeftSection>
-  </Section>
-);
+const Hero = (props) => {
+  const audioRef = useRef();
+
+  const handleLearnMore = () => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = 0;
+      audioRef.current.play();
+    }
+  }
+
+  return (
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Utkarsh Pathrabe
+        </SectionTitle>
+        <SectionText>
+          Senior System Software Engineer (Autonomous Vehicles) at NVIDIA
+        </SectionText>
+        <Link href='#about' passHref>
+          <Button onClick={handleLearnMore}>Learn More</Button>
+        </Link>
+      </LeftSection>
+      <audio ref={audioRef}>
+        <source src='/audio/guitar-beat-intro.mp3'></source>
+      </audio>
+    </Section>
+  );
+};
 
 export default Hero;
