1a6599fde483afab4f1b8577d907f2eeab6893d5
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-01 11:01:54 +0530

    Update ProjectsStyles.js

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -44,7 +44,7 @@ export const HeaderThree = styled.h3`
   letter-spacing: 2px;
   color: #9cc9e3;
   padding: .5rem 0;
-  font-size: ${(props) => props.title ? '3rem' : '2rem'};
+  font-size: ${(props) => props.isTitle ? '3rem' : '2rem'};
 `;
 
 export const Hr = styled.hr`
@@ -68,7 +68,7 @@ export const Intro = styled.div`
 
 export const CardInfo = styled.p`
   width: 100%;
-  padding: 0 2rem;
+  padding: 0 3rem;
   color: #e4e6e7;
   font-style: 2rem;
   line-height: 24px;
