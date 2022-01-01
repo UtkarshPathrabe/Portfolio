cfc02e624734bf6c43ad4f0b8586e86ce1a330d9
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-01 14:42:12 +0530

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,7 +6,7 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <Section row>
+  <Section row nopadding>
     <LeftSection>
       <SectionTitle main center>
         Utkarsh Pathrabe
