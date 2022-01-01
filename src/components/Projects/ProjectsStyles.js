b83f849ef206a47831d5d715c35270a7cddfeae1
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-01 14:34:59 +0530

    Update ProjectsStyles.js

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -26,6 +26,7 @@ row-gap: 3rem;
 export const BlogCard = styled.div`
   border-radius: 10px;
   box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
+  margin: 0 auto 20px;
   text-align: center;
   width: 400px;
   @media ${(props) => props.theme.breakpoints.sm} {
