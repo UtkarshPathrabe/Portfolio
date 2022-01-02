96a5686c1a36dc7336a30900a5f94f948467b50c
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-02 18:47:31 +0530

    Update SideDrawer.js

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from 'next/link';
-import { SideDrawerNavigation, SideDrawerNavigationItems, SideDrawerNavigationItem, NavLink } from './SideDrawerStyles';
+import { SideDrawerNavigation, SideDrawerNavigationItems, SideDrawerNavigationItem, NavLink, SocialIconsContainer, SocialIcons } from './SideDrawerStyles';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillFacebook, AiFillTwitterCircle } from 'react-icons/ai';
 import { toolbarItemsList } from '../../constants/toolbar';
 
 const SideDrawer = ({ isOpen, itemClickHandler }) => (
@@ -16,6 +17,23 @@ const SideDrawer = ({ isOpen, itemClickHandler }) => (
                 </SideDrawerNavigationItem>
             ))}
         </SideDrawerNavigationItems>
+        <SocialIconsContainer onClick={itemClickHandler}>
+            <SocialIcons href='https://github.com/UtkarshPathrabe' target='_blank'>
+                <AiFillGithub size='3rem' />
+            </SocialIcons>
+            <SocialIcons href='https://www.linkedin.com/in/utkarshpathrabe/' target='_blank'>
+                <AiFillLinkedin size='3rem' />
+            </SocialIcons>
+            <SocialIcons href='https://www.instagram.com/utkarsh_pathrabe/' target='_blank'>
+                <AiFillInstagram size='3rem' />
+            </SocialIcons>
+            <SocialIcons href='https://www.facebook.com/utkarshpathrabe/' target='_blank'>
+                <AiFillFacebook size='3rem' />
+            </SocialIcons>
+            <SocialIcons href='https://twitter.com/UtkarshPathrabe' target='_blank'>
+                <AiFillTwitterCircle size='3rem' />
+            </SocialIcons>
+        </SocialIconsContainer>
     </SideDrawerNavigation>
 );
 
