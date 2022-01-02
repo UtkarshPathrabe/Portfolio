a3d2aed60a2fc6d0f3e25f6fade56ac24bbace60
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-02 17:49:32 +0530

    Create toolbar.js

---
+++ src/constants/toolbar.js
@@ -0,0 +1,18 @@
+export const toolbarItemsList = [
+    {
+        label: 'Projects',
+        url: '#projects',
+    },
+    {
+        label: 'Technologies',
+        url: '#tech',
+    },
+    {
+        label: 'About',
+        url: '#about',
+    },
+    {
+        label: 'Paintings',
+        url: '#paintings',
+    }
+];
