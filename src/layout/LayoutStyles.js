6b7d9712ddd0fd8d7c888a671252a5710b7cf52f
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-02 17:49:37 +0530

    Update LayoutStyles.js

--- src/layout/LayoutStyles.js
+++ src/layout/LayoutStyles.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-max-width: 1280px;
-width: 100%;
-margin: auto;
+    max-width: 1280px;
+    width: 100%;
+    margin: auto;
+    height: 100%;
 `;
