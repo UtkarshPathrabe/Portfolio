59c114f0df5d881e5718f39db1662bf9bedde1db
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-02 17:49:34 +0530

    Update Layout.js

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-import Footer from '../components/Footer/Footer'
-import Header from '../components/Header/Header'
-import { Container } from './LayoutStyles'
+import Footer from '../components/Footer/Footer';
+import Header from '../components/Header/Header';
+import Toolbar from '../components/Toolbar/Toolbar';
+import SideDrawer from '../components/SideDrawer/SideDrawer';
+import Backdrop from '../components/Backdrop/Backdrop';
+import { Container } from './LayoutStyles';
 
 export const Layout = ({children}) => {
+  const [sideDrawerOpen, setSideDrawerOpen] = useState(false);
+
+  const drawerToggleClickHandler = () => {
+    setSideDrawerOpen(current => !current);
+  };
+
+  const backdropClickHandler = () => {
+    setSideDrawerOpen(false);
+  };
+
+  let backdrop;
+  if (sideDrawerOpen) {
+    backdrop = (<Backdrop onClickHandler={backdropClickHandler} />);
+  }
+
   return (
     <Container>
-     <Header/>
-     <main>{children}</main> 
-     <Footer/>
+      <Toolbar toggleButtonHandler={drawerToggleClickHandler} />
+      <SideDrawer isOpen={sideDrawerOpen} />
+      {backdrop}
+      <main style={{ marginTop: '64px' }}>
+        {children}
+      </main>
+      <Footer/>
     </Container>
   )
-}
+};
