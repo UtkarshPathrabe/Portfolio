295403585ea9bcd96bb55083cdd85e254ec99106
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-02 17:49:39 +0530

    Update index.js

--- src/styles/GlobalComponents/index.js
+++ src/styles/GlobalComponents/index.js
@@ -12,12 +12,12 @@ export const Section = styled.section`
   grid-template-columns: 1fr 1fr;
 
   @media ${(props) => props.theme.breakpoints.md} {
-    padding: 24px 48px 0;
+    padding: 32px 48px 0;
     flex-direction: column;
   }
 
   @media ${(props) => props.theme.breakpoints.sm} {
-    padding: ${(props) => props.nopadding ? "0" : "16px 16px 0" };
+    padding: ${(props) => props.nopadding ? "0" : "32px 16px 0" };
     width: calc(100vw - 32px);
     flex-direction: column;
   }
