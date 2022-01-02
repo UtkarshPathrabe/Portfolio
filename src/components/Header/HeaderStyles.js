ffa9e5664492c7202039859c75688748478e93a9
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-02 18:36:11 +0530

    Update HeaderStyles.js

--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -114,14 +114,13 @@ export const NavProductsIcon = styled(IoIosArrowDropdown)`
 // Social Icons 
 
 export const SocialIcons = styled.a`
-transition: 0.3s ease;
-color: white;
-border-radius: 50px;
+  transition: 0.3s ease;
+  color: white;
+  border-radius: 50px;
   padding: 8px;
-&:hover {
+  &:hover {
     background-color: #212d45;
     transform: scale(1.2);
     cursor: pointer;
-    
   }
 `
