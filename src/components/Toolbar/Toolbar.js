ca543d3326eaaa08083d284bf8fb63ebdda2c532
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-02 18:36:17 +0530

    Update Toolbar.js

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Link from 'next/link';
-import { ToolbarContainer, ToolbarNavigation, DrawerToggleButtonContainer, ToolbarLogo, ToolbarLogoNavLink, ToolbarNavigationItems, ToolbarNavigationItemsList, ToolbarNavigationItem, NavLink, Spacer } from './ToolbarStyles.js';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillFacebook, AiFillTwitterCircle } from 'react-icons/ai';
+
+import { ToolbarContainer, ToolbarNavigation, DrawerToggleButtonContainer, ToolbarLogo, ToolbarLogoNavLink, ToolbarNavigationItems, ToolbarNavigationItemsList, ToolbarNavigationItem, NavLink, Spacer, SocialIconsContainer, SocialIcons } from './ToolbarStyles.js';
 import { toolbarItemsList } from '../../constants/toolbar';
 import DrawerToggleButton from "../SideDrawer/DrawerToggleButton.js";
 
@@ -31,6 +33,24 @@ const Toolbar = ({ toggleButtonHandler }) => (
                     ))}
                 </ToolbarNavigationItemsList>
             </ToolbarNavigationItems>
+            <Spacer />
+            <SocialIconsContainer>
+                <SocialIcons href='https://github.com/UtkarshPathrabe' target='_blank'>
+                    <AiFillGithub size='3rem' />
+                </SocialIcons>
+                <SocialIcons href='https://www.linkedin.com/in/utkarshpathrabe/' target='_blank'>
+                    <AiFillLinkedin size='3rem' />
+                </SocialIcons>
+                <SocialIcons href='https://www.instagram.com/utkarsh_pathrabe/' target='_blank'>
+                    <AiFillInstagram size='3rem' />
+                </SocialIcons>
+                <SocialIcons href='https://www.facebook.com/utkarshpathrabe/' target='_blank'>
+                    <AiFillFacebook size='3rem' />
+                </SocialIcons>
+                <SocialIcons href='https://twitter.com/UtkarshPathrabe' target='_blank'>
+                    <AiFillTwitterCircle size='3rem' />
+                </SocialIcons>
+            </SocialIconsContainer>
         </ToolbarNavigation>
     </ToolbarContainer>
 );
