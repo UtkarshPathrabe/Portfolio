a1a4e16703654415b244d5b6d6073f0a55e10b2f
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-03 23:05:21 +0530

    Update NavDropDown.js

--- src/components/NavDropDown/NavDropDown.js
+++ src/components/NavDropDown/NavDropDown.js
@@ -25,7 +25,8 @@ export const DropDownContainer = styled.div`
   @media ${(props) => props.theme.breakpoints.sm} {
     top: 24px;
   }
-`
+`;
+
 export const DropDownItem = styled.a`
   width: 100%;
   display: flex;
@@ -47,25 +48,25 @@ export const DropDownItem = styled.a`
   &:nth-of-type(3n):hover {
     box-shadow: 0 -3px 6px 3px rgba(0,0,0,.3);
   }
-`
+`;
 
 export const DropDownIcon = styled.div`
   width: 32px;
   height: 32px;
   margin-right: 16px;
-`
+`;
 
 export const DropDownTextContainer = styled.div`
   display: flex;
   flex-direction: column;
-`
+`;
 
 export const DropDownItemTitle = styled.h2`
   color: #0f1624;
   font-size: 18px;
   line-height: 26px;
   text-align: start;
-`
+`;
 
 export const DropDownItemDesc = styled.p`
   color: #0f1624;
@@ -73,4 +74,4 @@ export const DropDownItemDesc = styled.p`
   font-size: 14px;
   line-height: 22px;
   text-align: start;
-`
+`;
