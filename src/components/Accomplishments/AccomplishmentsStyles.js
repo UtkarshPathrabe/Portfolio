f01cf490d0e4c449f9849f8090a358c688b563c9
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-07 00:40:03 +0530

    Update AccomplishmentsStyles.js

--- src/components/Accomplishments/AccomplishmentsStyles.js
+++ src/components/Accomplishments/AccomplishmentsStyles.js
@@ -3,44 +3,35 @@ import styled from "styled-components";
 export const Boxes = styled.div`
   width: 100%;
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: 1fr 1fr;
   gap: 24px;
   margin: 24px 0 40px;
 
   @media ${props => props.theme.breakpoints.md}{
     gap: 16px;
     margin: 20px 0 32px;
-    grid-template-columns: repeat(auto-fit, minmax(140px, 1fr));
+    grid-template-columns: 1fr 1fr;
   }
 
   @media ${props => props.theme.breakpoints.sm}{
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: 1fr;
     gap: 10px;
-    max-width: 500px;
-    margin: 24px auto;
+    margin: 18px auto;
   }
 `;
 
 export const Box = styled.div`
   background: #212D45;
   border-radius: 12px;
-  height: 144px;
+  height: 100%;
   padding: 24px;
-  @media ${props => props.theme.breakpoints.lg} {
-    height: 210px;
-
-  }
 
   @media ${props => props.theme.breakpoints.md} {
-    height: 135px;
     padding: 16px;
   }
 
   @media ${props => props.theme.breakpoints.sm} {
-    height: 110px;
     padding: 12px;
-    
     &:nth-child(2n){
       grid-row:2;
     }
@@ -84,3 +75,5 @@ export const BoxText = styled.p`
     line-height: 14px;
   }
 `;
+
+export const GitHubStats = styled.img``;
