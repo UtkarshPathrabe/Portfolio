c549960521cc4cf392ef64a27cbb7d5de8374cf5
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-07 00:39:59 +0530

    Update Accomplishments.js

--- src/components/Accomplishments/Accomplishments.js
+++ src/components/Accomplishments/Accomplishments.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { Box, Boxes, BoxNum, BoxText } from './AccomplishmentsStyles';
+import { Box, Boxes, BoxNum, BoxText, GitHubStats } from './AccomplishmentsStyles';
 import LeetCode from './LeetCode/LeetCode';
 import HackerRank from './HackerRank/HackerRank';
 import SphereOnlineJudge from './SphereOnlineJudge/SphereOnlineJudge';
@@ -10,13 +10,13 @@ import Certificates from './Certificates/Certificates';
 const data = [
   { number: 20, text: 'Open Source Projects'},
   { number: 190, text: 'Github Followers', },
-  { number: 390, text: 'Github Stars', }
 ];
 
 const Accomplishments = () => (
   <Section id='accomplishments'>
     <SectionDivider />
     <SectionTitle main>Accomplishments</SectionTitle>
+    <GitHubStats src={'https://github-readme-stats.vercel.app/api?username=UtkarshPathrabe&show_icons=true&hide=prs,issues,contribs&theme=github_dark&border_radius=10px'} alt='github_stats' />
     <Boxes>
       {data.map(({ number, text }, index) => (
         <Box key={index}>
