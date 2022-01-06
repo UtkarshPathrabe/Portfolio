b31d4e59764133844d59528874848c57021b5949
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-06 21:27:20 +0530

    Update Accomplishments.js

--- src/components/Accomplishments/Accomplishments.js
+++ src/components/Accomplishments/Accomplishments.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { Box, Boxes, BoxNum, BoxText } from './AccomplishmentsStyles';
+import { Section, SectionDivider, SectionTitle, SectionSubTitle } from '../../styles/GlobalComponents';
+import { Box, Boxes, BoxNum, BoxText, CertificateContainer, CertificateLink, CertificateCard, Title, Text, HelperText } from './AccomplishmentsStyles';
 import LeetCode from './LeetCode/LeetCode';
 import HackerRank from './HackerRank/HackerRank';
 import SphereOnlineJudge from './SphereOnlineJudge/SphereOnlineJudge';
+import CertificateData from '../../constants/certificates';
 
 const data = [
   { number: 20, text: 'Open Source Projects'},
@@ -17,16 +18,31 @@ const Accomplishments = () => (
     <SectionDivider />
     <SectionTitle main>Accomplishments</SectionTitle>
     <Boxes>
-      {data.map((card, index) => (
+      {data.map(({ number, text }, index) => (
         <Box key={index}>
-          <BoxNum>{card.number}+</BoxNum>
-          <BoxText>{card.text}</BoxText>
+          <BoxNum>{number}+</BoxNum>
+          <BoxText>{text}</BoxText>
         </Box>
       ))}
     </Boxes>
     <LeetCode />
     <HackerRank />
     <SphereOnlineJudge />
+    <SectionSubTitle>Certificates</SectionSubTitle>
+    <CertificateContainer>
+      {CertificateData.map(({ name, organization, issue_date, certificate_id, certificate_url }) => (
+        <CertificateLink key={name} href={certificate_url} target='_blank'>
+          <CertificateCard>
+            <Title>{name}</Title>
+            <div>
+              <Text>{organization}</Text>
+              <Text>Issue Date: {issue_date}</Text>
+              {certificate_id && <HelperText>ID: {certificate_id}</HelperText>}
+            </div>
+          </CertificateCard>
+        </CertificateLink>
+      ))}
+    </CertificateContainer>
   </Section>
 );
 
