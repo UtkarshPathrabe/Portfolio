1fbbc13d37a60f37c5bd411ac6839d62590b1576
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-06 19:25:13 +0530

    Update SphereOnlineJudgeStyles.js

--- src/components/Accomplishments/SphereOnlineJudge/SphereOnlineJudgeStyles.js
+++ src/components/Accomplishments/SphereOnlineJudge/SphereOnlineJudgeStyles.js
@@ -9,14 +9,7 @@ export const Content = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    min-height: 15rem;
-    margin: 24px 0 40px;
-    @media ${props => props.theme.breakpoints.md}{
-        margin: 20px 0 32px;
-    }
-    @media ${props => props.theme.breakpoints.sm}{
-        margin: 24px auto;
-    }
+    margin-bottom: 1rem;
 `;
 
 export const ContentContainer = styled.div`
