73bb88081c67f67ca26c185c0f7911d955493cb9
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-06 10:26:27 +0530

    Update utils.js

--- src/utilities/utils.js
+++ src/utilities/utils.js
@@ -1,1 +1,3 @@
-export const getDateDiffInDays = (date2, date1) => Math.ceil(Math.abs(new Date(date2 * 1000) - new Date(date1 * 1000)) / (1000 * 60 * 60 * 24),);
+export const getDateDiffInDays = (date2, date1) => Math.ceil(Math.abs(new Date(date2 * 1000) - new Date(date1 * 1000)) / (1000 * 60 * 60 * 24));
+
+export const fetcher = (url) => fetch(url).then((res) => res.json());
