0504f7112aa76c8d90aef17ef4208eb22bb5c47e
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-07 11:45:35 +0530

    Create ScrollButtonStyles.js

---
+++ src/components/ScrollButton/ScrollButtonStyles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+  
+export const Button = styled.div`
+    position: fixed; 
+    width: 100%;
+    left: calc(100% - 60px);
+    bottom: 20px;
+    height: 4rem;
+    font-size: 4rem;
+    z-index: 1;
+    cursor: pointer;
+    outline: none;
+    background: transparent;
+`;
