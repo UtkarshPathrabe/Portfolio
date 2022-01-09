2ecead84c6ecf6056cce47f176f6259837bc0bb7
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-09 13:27:39 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,43 +1,38 @@
 import React from 'react';
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/projects';
 
 const Projects = () => (
-  <Section id='projects'>
-    <SectionDivider />
-    <SectionTitle main>Projects</SectionTitle>
-    <GridContainer>
-      {projects.map(( { id, image, title, description, tags, source, visit } ) => (
-        <BlogCard key={id}>
-          <Img src={image} alt={id} />
-          <TitleContent>
-            <HeaderThree isTitle={true}>
-              {title}
-            </HeaderThree>
-            <Hr />
-          </TitleContent>
-          <CardInfo>{description}</CardInfo>
-          <div>
-            <TitleContent>Stack</TitleContent>
-            <TagList>
-              {tags.map((tag, index) => (
-                <Tag key={index}>{tag}</Tag>
-              ))}
-            </TagList>
-          </div>
-          <UtilityList>
-            <ExternalLinks href={visit} target='_blank'>
-              Visit
-            </ExternalLinks>
-            <ExternalLinks href={source} target='_blank'>
-              Source
-            </ExternalLinks>
-          </UtilityList>
-        </BlogCard>
-      ))}
-    </GridContainer>
-  </Section>
+  <GridContainer>
+    {projects.map(( { id, image, title, description, tags, source, visit } ) => (
+      <BlogCard key={id}>
+        <Img src={image} alt={id} />
+        <TitleContent>
+          <HeaderThree isTitle={true}>
+            {title}
+          </HeaderThree>
+          <Hr />
+        </TitleContent>
+        <CardInfo>{description}</CardInfo>
+        <div>
+          <TitleContent>Stack</TitleContent>
+          <TagList>
+            {tags.map((tag, index) => (
+              <Tag key={index}>{tag}</Tag>
+            ))}
+          </TagList>
+        </div>
+        <UtilityList>
+          <ExternalLinks href={visit} target='_blank'>
+            Visit
+          </ExternalLinks>
+          <ExternalLinks href={source} target='_blank'>
+            Source
+          </ExternalLinks>
+        </UtilityList>
+      </BlogCard>
+    ))}
+  </GridContainer>
 );
 
 export default Projects;
