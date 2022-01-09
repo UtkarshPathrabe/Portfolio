09b4c21e65f6a74ee859271d81994a5c26bc5e96
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-09 14:34:04 +0530

    Update DrawerToggleButton.js

--- src/components/SideDrawer/DrawerToggleButton.js
+++ src/components/SideDrawer/DrawerToggleButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import { ToggleButton, ToggleButtonLine } from './DrawerToggleButtonStyles';
 
 const DrawerToggleButton = ({ onClickHandler }) => (
-    <ToggleButton onClick={onClickHandler}>
+    <ToggleButton onClick={onClickHandler} aria-label='toggle-button'>
         <ToggleButtonLine />
         <ToggleButtonLine />
         <ToggleButtonLine />
