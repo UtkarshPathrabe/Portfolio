f98cdebe8d0ae20d865131ed347e6ec09f404a99
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-09 14:15:07 +0530

    Update SideDrawer.js

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -25,20 +25,20 @@ const SideDrawer = ({ isOpen, itemClickHandler, handleLinkClick }) => (
             </AccordionContext.Consumer>
         </SideDrawerNavigationItems>
         <SocialIconsContainer onClick={itemClickHandler}>
-            <SocialIcons href='https://github.com/UtkarshPathrabe' target='_blank' rel='noopener noreferrer'>
-                <AiFillGithub size='3rem' alt='github' />
+            <SocialIcons href='https://github.com/UtkarshPathrabe' target='_blank' rel='noopener noreferrer' aria-label='github'>
+                <AiFillGithub size='3rem' />
             </SocialIcons>
-            <SocialIcons href='https://www.linkedin.com/in/utkarshpathrabe/' target='_blank' rel='noopener noreferrer'>
-                <AiFillLinkedin size='3rem' alt='linkedin' />
+            <SocialIcons href='https://www.linkedin.com/in/utkarshpathrabe/' target='_blank' rel='noopener noreferrer' aria-label='linkedin'>
+                <AiFillLinkedin size='3rem' />
             </SocialIcons>
-            <SocialIcons href='https://www.instagram.com/utkarsh_pathrabe/' target='_blank' rel='noopener noreferrer'>
-                <AiFillInstagram size='3rem' alt='instagram' />
+            <SocialIcons href='https://www.instagram.com/utkarsh_pathrabe/' target='_blank' rel='noopener noreferrer' aria-label='instagram'>
+                <AiFillInstagram size='3rem' />
             </SocialIcons>
-            <SocialIcons href='https://www.facebook.com/utkarshpathrabe/' target='_blank' rel='noopener noreferrer'>
-                <AiFillFacebook size='3rem' alt='facebook' />
+            <SocialIcons href='https://www.facebook.com/utkarshpathrabe/' target='_blank' rel='noopener noreferrer' aria-label='facebook'>
+                <AiFillFacebook size='3rem' />
             </SocialIcons>
-            <SocialIcons href='https://twitter.com/UtkarshPathrabe' target='_blank' rel='noopener noreferrer'>
-                <AiFillTwitterCircle size='3rem' alt='twitter' />
+            <SocialIcons href='https://twitter.com/UtkarshPathrabe' target='_blank' rel='noopener noreferrer' aria-label='twitter'>
+                <AiFillTwitterCircle size='3rem' />
             </SocialIcons>
         </SocialIconsContainer>
     </SideDrawerNavigation>
