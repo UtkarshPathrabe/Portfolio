c1cd8f3d09d5f7f61f244882c173d9438a3a9765
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-09 13:41:41 +0530

    Update FooterStyles.js

--- src/components/Footer/FooterStyles.js
+++ src/components/Footer/FooterStyles.js
@@ -158,7 +158,7 @@ export const LinkTitle = styled.h4`
 	font-size: 12px;
 	line-height: 24px;
 	text-transform: uppercase;
-	color: rgba(255, 255, 255, 0.4);
+	color: rgba(255, 255, 255, 0.5);
 	margin-bottom: 16px;
 
 	@media ${props => props.theme.breakpoints.sm} {
