9725af33b473378428a292a7000356047a3a677b
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-09 13:27:25 +0530

    Create AccordionStyles.js

---
+++ src/components/Accordion/AccordionStyles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const AccordionTitleContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
+    transition: 0.6s ease;
+`;
