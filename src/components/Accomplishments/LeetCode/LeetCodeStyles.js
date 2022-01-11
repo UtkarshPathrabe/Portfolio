33a4cc456a38c31dba04b75fd23c3e44690e6b79
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-11 20:26:06 +0530

    Update LeetCodeStyles.js

--- src/components/Accomplishments/LeetCode/LeetCodeStyles.js
+++ src/components/Accomplishments/LeetCode/LeetCodeStyles.js
@@ -61,3 +61,85 @@ export const ContentContainer = styled.div`
     flex-direction: column;
     width: 100%;
 `;
+
+export const BadgesContainer = styled.div`
+    margin: 2rem auto;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const BadgeParentContainer = styled.div`
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    column-gap: 2rem;
+    row-gap: 2rem;
+    @media ${(props) => props.theme.breakpoints.lg} {
+        grid-template-columns: 1fr 1fr;
+    }
+    @media ${(props) => props.theme.breakpoints.sm} {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+`;
+
+export const Badge = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
+    box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
+    padding: 10px;
+`;
+
+export const BadgeImage = styled.img`
+    width: 60%;
+    background-color: ${(props) => props.backgroundColor};
+    border-radius: 10px;
+    padding: 30px;
+    @media ${(props) => props.theme.breakpoints.sm} {
+        padding: 20px;
+    }
+`;
+
+export const Title = styled.h4`
+    font-size: 26px;
+    line-height: 40px;
+    font-weight: 300;
+    color: rgba(255, 255, 255, 0.5);
+    padding-bottom: 1rem;
+
+    @media ${(props) => props.theme.breakpoints.md} {
+        font-size: 22px;
+        line-height: 32px;
+    }
+
+    @media ${(props) => props.theme.breakpoints.sm} {
+        font-size: 18px;
+        line-height: 24px;
+    }
+`;
+
+export const Text = styled.p`
+    font-size: 24px;
+    line-height: 40px;
+    font-weight: 200;
+    color: rgba(255, 255, 255, 0.5);
+
+    @media ${(props) => props.theme.breakpoints.md} {
+        font-size: 20px;
+        line-height: 32px;
+    }
+
+    @media ${(props) => props.theme.breakpoints.sm} {
+        font-size: 16px;
+        line-height: 24px;
+    }
+`;
