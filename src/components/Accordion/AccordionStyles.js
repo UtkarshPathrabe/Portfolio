b49c43c07261c76fbf0603ef8d7ecf4e81901d04
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-12 14:33:12 +0530

    Update AccordionStyles.js

--- src/components/Accordion/AccordionStyles.js
+++ src/components/Accordion/AccordionStyles.js
@@ -7,3 +7,38 @@ export const AccordionTitleContainer = styled.div`
     cursor: pointer;
     transition: 0.6s ease;
 `;
+
+export const AccordionCollapseButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    transition: 0.6s ease;
+`;
+
+export const AccordionCollapseButton = styled.button`
+border: none;
+border-radius: 50px;
+color: #fff;
+display: flex;
+padding: 1rem;
+background: linear-gradient(270deg, #13ADC7 0%, #945DD6 100%);
+font-size: 24px;
+font-weight: 300;
+align-items: center;
+justify-content: center;
+cursor: pointer;
+box-shadow: none;
+
+&:focus {
+  outline: none;
+}
+
+@media ${(props) => props.theme.breakpoints.md} {
+  font-size: 20px;
+}
+
+@media ${(props) => props.theme.breakpoints.sm} {
+  font-size: 16px;
+}
+`;
