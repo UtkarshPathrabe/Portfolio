c0bef3fd6f242be3ad88b04e648a27fba18f3717
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-12 14:54:38 +0530

    Update AccordionStyles.js

--- src/components/Accordion/AccordionStyles.js
+++ src/components/Accordion/AccordionStyles.js
@@ -5,7 +5,6 @@ export const AccordionTitleContainer = styled.div`
     justify-content: space-between;
     align-items: center;
     cursor: pointer;
-    transition: 0.6s ease;
 `;
 
 export const AccordionCollapseButtonContainer = styled.div`
@@ -13,32 +12,32 @@ export const AccordionCollapseButtonContainer = styled.div`
     justify-content: center;
     align-items: center;
     width: 100%;
-    transition: 0.6s ease;
+    margin-top: 8px;
 `;
 
 export const AccordionCollapseButton = styled.button`
-border: none;
-border-radius: 50px;
-color: #fff;
-display: flex;
-padding: 1rem;
-background: linear-gradient(270deg, #13ADC7 0%, #945DD6 100%);
-font-size: 24px;
-font-weight: 300;
-align-items: center;
-justify-content: center;
-cursor: pointer;
-box-shadow: none;
+    border: none;
+    border-radius: 50px;
+    color: #fff;
+    display: flex;
+    padding: 1rem;
+    background: linear-gradient(270deg, #13ADC7 0%, #945DD6 100%);
+    font-size: 24px;
+    font-weight: 300;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    box-shadow: none;
 
-&:focus {
-  outline: none;
-}
+    &:focus {
+        outline: none;
+    }
 
-@media ${(props) => props.theme.breakpoints.md} {
-  font-size: 20px;
-}
+    @media ${(props) => props.theme.breakpoints.md} {
+        font-size: 20px;
+    }
 
-@media ${(props) => props.theme.breakpoints.sm} {
-  font-size: 16px;
-}
+    @media ${(props) => props.theme.breakpoints.sm} {
+        font-size: 16px;
+    }
 `;
