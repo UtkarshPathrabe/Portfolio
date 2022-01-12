10c311ddd9671e3d00af43dffdae31de57a252e3
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-12 14:33:10 +0530

    Update Accordion.js

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,16 +1,26 @@
-import { AccordionTitleContainer } from './AccordionStyles';
+import Link from 'next/link';
+
+import { AccordionTitleContainer, AccordionCollapseButtonContainer, AccordionCollapseButton } from './AccordionStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { AiOutlineCaretRight, AiOutlineCaretDown } from 'react-icons/ai';
+import { AiOutlineCaretRight, AiOutlineCaretDown, AiOutlineCaretUp } from 'react-icons/ai';
 
 const Accordion = ({ title, content, id, isActive, toggleActive }) => {
     return (
         <Section id={id}>
             <SectionDivider />
-            <AccordionTitleContainer onClick={toggleActive}>
-                <SectionTitle main>{ title }</SectionTitle>
-                <SectionTitle main>{ isActive ? <AiOutlineCaretDown /> : <AiOutlineCaretRight /> }</SectionTitle>
-            </AccordionTitleContainer>
+            <Link href={`#${id}`} passHref>
+                <AccordionTitleContainer onClick={toggleActive}>
+                    <SectionTitle main>{ title }</SectionTitle>
+                    <SectionTitle main>{ isActive ? <AiOutlineCaretDown /> : <AiOutlineCaretRight /> }</SectionTitle>
+                </AccordionTitleContainer>
+            </Link>
             { isActive && content }
+            { isActive && 
+            (<AccordionCollapseButtonContainer>
+                <Link href={`#${id}`} passHref>
+                    <AccordionCollapseButton onClick={toggleActive}>Collapse {title} <AiOutlineCaretUp /></AccordionCollapseButton>
+                </Link>
+            </AccordionCollapseButtonContainer>) }
         </Section>
     );
 };
